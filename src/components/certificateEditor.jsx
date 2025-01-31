c0d402be466cb9826ba72c7136a07416f61db865
Riddhi Chavan <riddhis1999@gmail.com>
2025-01-31 22:53:34 +0530

    Modified Comments

--- src/components/certificateEditor.jsx
+++ src/components/certificateEditor.jsx
@@ -4,6 +4,7 @@ import Input from '../components/ui/input';
 import Button from '../components/ui/button';
 import { Download, Upload } from 'lucide-react';
 import html2canvas from 'html2canvas';
+//import canvas
 
 
 
